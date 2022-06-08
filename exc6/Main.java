2d8c55f7165256f4b777d4e858fd0051b1f6b467
AlexandreMelo22 <88670281+AlexandreMelo22@users.noreply.github.com>
2022-06-07 21:59:32 -0300

    Create Main.java

---
+++ exc6/Main.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+        Stack<Integer> pilha = new Stack<>();
+        Random rdm = new Random();
+        
+        int i=0;
+        
+        //Os 1000 positivos na fila
+        while(i<1000){
+            int numAleatorio = rdm.nextInt();
+            if(numAleatorio >= 0){
+                fila.push(numAleatorio);
+                i++;
+            }
+        }
+        
+        i=0;
+        
+        // Os 1000 negativos na fila
+        while(i<1000){
+            int numAleatorio = rdm.nextInt();
+            if(numAleatorio < 0){
+                fila.push(numAleatorio);
+                i++;
+            }
+        }
+        
+        i=0;
+        
+        while(!fila.empty()){
+            if(fila.peek() > 0){
+                pilha.push(fila.pop());
+            }
+            else{
+                System.out.println(i + ". " + pilha.pop());
+                i++;
+                fila.pop();
+            }
+            
+        }
+    }
+
+}
